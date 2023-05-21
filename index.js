017efcc56cd0d72d3dd471af5ed2ef5164f0d4da
Lewis Kirega <lewismk744@gmail.com>
2023-05-21 23:34:35 +0300

    change css, html and javascript

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+//increment button & save
+let saveEl= document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let subwaycount = 0
 
 function increment() {
-    subwaycount = subwaycount + 1
-    countEl.innerText = subwaycount
+    subwaycount += 1
+    countEl.textContent = subwaycount
     console.log("click")
     console.log(subwaycount)
 }
+
+// save button
+
+function save(){
+    let subwaystr = subwaycount + " - "
+    saveEl.textContent += subwaystr
+    countEl.textContent= 0
+    subwaycount=0
+    console.log("click")
+    console.log(subwaystr)
+}
+
